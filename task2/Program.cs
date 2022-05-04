bcdd6228e8291e1c02fbddd750306dbf65bb325c
mike_karni <3923499@mail.ru>
2022-05-04 08:11:56 +0300

    Amend

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,15 +2,9 @@
 С помощью команд он может попросить сконвертировать одну валюту в другую. 
 Курс конвертации просто описать в программе. 
 Программа заканчивает свою работу в тот момент, когда решит пользователь.*/
-/*1. запросить введите валюту
-$ /евро/ Бел.Рубли
-показать курс обмена.*/
-/*Console.WriteLine("Введите валюту , которую вы ходите обменять на российские рубли: евро, доллар,белорусский рубль");
-string currency = Console.ReadLine();
-Console.WriteLine("курс евро/российский рубль = 74,56");
-Console.WriteLine("курс доллар/российский рубль = 71,02");
-Console.WriteLine("курс белорусский рубль/российский рубль = 26,77");*/
+/*1. запросить введите валюту*/
 
+// Портфель
 Console.WriteLine("Введите  ваш баланс валют");
 Console.Write("Баланс евро ");
 double balanceEvro = Convert.ToDouble(Console.ReadLine());
@@ -18,76 +12,106 @@ Console.Write("Баланс  доллар ");
 double balanceUsd = Convert.ToDouble(Console.ReadLine());
 Console.Write("белорусский рубль ");
 double balanceBelRub = Convert.ToDouble(Console.ReadLine());
+
+//Вывод баланс
 Console.WriteLine($"Ваш баланс валют : \n{balanceEvro} евро. \n{balanceUsd} USD. \n{balanceBelRub} руб");
+// ВВОД ЧТО ХОТИМ ПОМЕНЯТЬ
 Console.WriteLine("Какую валюту из вашего баланса вы хотите поменять?");
 string wantToChange = Console.ReadLine();
+//ПРОВЕРКА ТОГО,ЧТО ХОТИМ МЕНЯТЬ ПО НАЗВАНИЮ
+while (wantToChange != "евро" && wantToChange != "доллар" && wantToChange != "белорусский рубль")
+{
+    Console.WriteLine("Введите правильное название валюты:евро / доллар / белорусский рубль");
+    wantToChange = Console.ReadLine();
+}
+// ВВОД НА ЧТО ХОТИМ МЕНЯТЬ
 Console.WriteLine($"На какую валюту  вы хотите поменять {wantToChange} ?");
 string changeCurrency = Console.ReadLine();
-
-if (wantToChange == "евро") 
+// ПРОВЕРКА НА ВВОД
+while (changeCurrency != "евро" && changeCurrency != "доллар" && changeCurrency != "белорусский рубль")
 {
-    if (changeCurrency == "доллар")
-    {
-
-        balanceUsd = balanceEvro * 1.05;
-        Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceUsd} долларов");
-    }
-    else if (wantToChange == "евро") 
-    {
-        if (changeCurrency == "белорусский рубль")
-        {
-            balanceBelRub = balanceEvro * 2.78;
-            Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceBelRub} долларов");
-        }
-    }
+    Console.WriteLine("Введите правильное название валюты:евро / доллар / белорусский рубль");
+    changeCurrency = Console.ReadLine();
 }
-if (wantToChange == "белорусский рубль") 
+
+
+
+if (wantToChange == "евро" && changeCurrency == "доллар")
 {
-    if (changeCurrency == "евро")
-    {
-
-        balanceEvro = balanceBelRub * 0.36;
-        Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceEvro} евро");
-    }
-    else if (wantToChange == "белорусский рубль") 
-    {
-        if (changeCurrency == "доллар")
-        {
-
-            balanceUsd = balanceBelRub * 0.38;
-            Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceUsd} долларов");
-        }
-    }
- }
-if (wantToChange == "евро") 
-{
-    if (changeCurrency == "доллар")
-    {
-
-        balanceUsd = balanceEvro * 1.05;
-        Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceUsd} долларов");
-    }
-    else if (wantToChange == "евро") 
-    {
-        if (changeCurrency == "белорусский рубль")
-        {
-
-            balanceBelRub = balanceEvro * 2.78;
-            Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceBelRub} долларов");
-        }
-    }  
+    Console.WriteLine("Какое количество евро вы хотите обменять на доллары? ");
+    double countchangeEvro = Convert.ToDouble(Console.ReadLine());   
+    balanceUsd = balanceUsd + countchangeEvro * 1.05;
+    balanceEvro = balanceEvro - countchangeEvro;
+    Console.WriteLine($"Ваш баланс портфеля после обмена  {wantToChange} на {changeCurrency} равен \n{balanceUsd} долларов, \n{balanceEvro}, \n{balanceBelRub}");    
 }
-if (wantToChange == changeCurrency) 
-    {    
-         Console.WriteLine($"А смысл менять {wantToChange} на {changeCurrency}?!!");       
+/*  else if (wantToChange == "евро")
+  {
+      if (changeCurrency == "белорусский рубль")
+      {
+          balanceBelRub = balanceEvro * 2.78;
+
+          Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceBelRub} долларов");
+      }
+  }
+}
+if (wantToChange == "белорусский рубль")
+{
+  if (changeCurrency == "евро")
+  {
+
+      balanceEvro = balanceBelRub * 0.36;
+      Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceEvro} евро");
+  }
+  else if (wantToChange == "белорусский рубль")
+  {
+      if (changeCurrency == "доллар")
+      {
+
+          balanceUsd = balanceBelRub * 0.38;
+          Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceUsd} долларов");
+      }
+  }
+}
+if (wantToChange == "евро")
+{
+  if (changeCurrency == "доллар")
+  {
+
+      balanceUsd = balanceEvro * 1.05;
+      Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceUsd} долларов");
+  }
+  else if (wantToChange == "евро")
+  {
+      if (changeCurrency == "белорусский рубль")
+      {
+
+          balanceBelRub = balanceEvro * 2.78;
+          Console.WriteLine($"Ваш баланс после обмена {wantToChange} на {changeCurrency} равен {balanceBelRub} долларов");
+      }
+  }
+}*/
+if (wantToChange == changeCurrency)
+{
+  Console.WriteLine($"А смысл менять {wantToChange} на {changeCurrency}?!!");
+}
+
+
+
+
+
+
+/*while (true)
+{
+    Console.WriteLine("Вы хотите продолжить обмен валют?");
+    string answer = Console.ReadLine();
+    if (answer == "да")
+    {
+
     }
-
-
-
-
-
-
-
+    else
+    {
+        break;
+    }*/
 
 
 
