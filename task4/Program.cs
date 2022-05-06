8ec27781a350f691a76e93fe770f97736bc5e859
mike_karni <3923499@mail.ru>
2022-05-06 19:05:05 +0300

    РЕШЕНА_ЗАДАЧА

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,54 +1,56 @@
-﻿void Insert (ref int[] array,int value,int index)
+﻿void Insert(ref int[] array, int value, int index)
 {
-    int[] newArray = new int[array.Length+1];
+    int[] newArray = new int[array.Length + 1];
     newArray[index] = value;
-    for(int i =0;i<index;i++)
+    for (int i = 0; i < index; i++)
     {
         newArray[i] = array[i];
     }
-    for(int i=index;i<array.Length;i++)
+    for (int i = index; i < array.Length; i++)
     {
-        newArray[i+1] = array[i];
+        newArray[i + 1] = array[i];
     }
-    array=newArray;
+    array = newArray;
 }
- Console.WriteLine("Введите длину массива ");
- int Length = Convert.ToInt32(Console.ReadLine());
- int[] numbers = new int[Length];
+Console.WriteLine("Введите длину массива ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Length];
 Console.WriteLine("Введите число , которое хотите добавить в массив");
- string newNumber = Console.ReadLine();
- int addedNumber = Convert.ToInt32(newNumber);
-Console.WriteLine("Введите на который хотите записать новое число");
+string newNumber = Console.ReadLine();
+int addedNumber = Convert.ToInt32(newNumber);
+Console.WriteLine("Введите  индекс на который хотите записать новое число");
 int position = Convert.ToInt32(Console.ReadLine());
-
- FillArray(numbers);
- PrintArray(numbers);
-Insert(ref numbers,addedNumber,position);
-NewFillArray(numbers);
+if(position>=0 && position<Length)
+{
+Console.WriteLine("Заполните исходный массив");
+FillArray(numbers);
+Console.WriteLine("Исходный массив : ");
 PrintArray(numbers);
+Insert(ref numbers, addedNumber, position);
+Console.WriteLine("Изменённый массив");
+PrintArray(numbers);
+}
+else
+{
+    Console.WriteLine("Вы ввели недопустимый индекс, на который хотите записать число.");
+}
 
-  void NewFillArray(int[] array)
- {
-    for (int i = 0; i < array.Length-1; i++)
-    {
-        array[i] = new Random().Next(100, 1000);
-        array[position] = addedNumber;
-     }
- }
- void FillArray(int[] array)
- {
+
+void FillArray(int[] array)
+{
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(100, 1000);
-     }
- }
+        string chislo = Console.ReadLine();
+        int chislo1 = Convert.ToInt32(chislo);
+        array[i] = chislo1;
+    }
+}
 
-
- void PrintArray(int[] array1)
- {
-     for (int i = 0; i < array1.Length; i++)
-     {
-         Console.Write(array1[i] + " " );
-     }
-     Console.WriteLine();
- }
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write(array1[i] + " ");
+    }
+    Console.WriteLine();
+}
