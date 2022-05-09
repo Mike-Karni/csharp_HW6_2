3d505c62dea3ca6664339332f01e14f456fc3a2e
mike_karni <3923499@mail.ru>
2022-05-09 19:16:17 +0300

    Done!

--- tasl5/Program.cs
+++ tasl5/Program.cs
@@ -53,37 +53,3 @@ void PrintArray(int[] array1)
 
 
 
-/*Console.WriteLine("Введите длину массива ");
-int Length = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[Length];
-Console.WriteLine("Введите число , которое хотите добавить в массив");
-string newNumber = Console.ReadLine();
-int addedNumber = Convert.ToInt32(newNumber);
-Console.WriteLine("Введите  индекс на который хотите записать новое число");
-int position = Convert.ToInt32(Console.ReadLine());
-if(position>=0 && position<Length)
-{
-Console.WriteLine("Заполните исходный массив");
-FillArray(numbers);
-Console.WriteLine("Исходный массив : ");
-PrintArray(numbers);
-Insert(ref numbers, addedNumber, position);
-Console.WriteLine("Изменённый массив");
-PrintArray(numbers);
-}
-else
-{
-    Console.WriteLine("Вы ввели недопустимый индекс, на который хотите записать число.");
-}
-
-void FillArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        string chislo = Console.ReadLine();
-        int chislo1 = Convert.ToInt32(chislo);
-        array[i] = chislo1;
-    }
-}*/
-
-
